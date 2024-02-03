c06e3cca03b8b6b3e93ce17314e60e4598b95029
Nawamin M. <naminx@gmail.com>
2024-02-03 11:55:59 +0700

    initial commit

---
+++ aliases.nix
@@ -0,0 +1,22 @@
+{
+  cat = "bat";
+  cd = "z";
+  cls = "clear";
+  copy = "cp -i";
+  cp = "cp -i";
+  del = "rm -i";
+  dir = "ansi lsd -lF --icon=never";
+  fe = "vi $(fzf --ansi --color=16)";
+  find = "fd";
+  fzf = "fzf --ansi --color=16";
+  grep = "rg";
+  ls = "ansi ls -CF --width=$(tput cols)";
+  md = "mkdir";
+  move = "mv -i";
+  mv = "mv -i";
+  rd = "rmdir";
+  ren = "mv -i";
+  rm = "rm -i";
+  seq2 = "seq -f %02g";
+  seq3 = "seq -f %03g";
+}
