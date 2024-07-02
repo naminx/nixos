722feb4cb2385bb54cd06d04ea58ff265702c78e
Nawamin M. <naminx@gmail.com>
2024-07-03 06:09:44 +0700

    Good config 3 Jul 2024

--- packages/onlyoffice/onlyoffice.nix
+++ packages/onlyoffice/onlyoffice.nix
@@ -1,12 +1,13 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
-
-let
-
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
   cfg = config.programs.onlyoffice;
 
-  x11Fonts = pkgs.runCommand "X11-fonts" { preferLocalBuild = true; } ''
+  x11Fonts = pkgs.runCommand "X11-fonts" {preferLocalBuild = true;} ''
     mkdir -p "$out"
     font_regexp='.*\.\(ttf\|ttc\|otf\|pcf\|pfa\|pfb\|bdf\)\(\.gz\)?'
     find ${toString config.fonts.packages} -regex "$font_regexp" \
@@ -18,28 +19,61 @@ let
     cat $(find ${pkgs.xorg.fontalias}/ -name fonts.alias) >fonts.alias
   '';
 
-  mkFHSEnv = onlyofficeUnwrapped: pkgs.buildFHSUserEnvBubblewrap (rec {
-    name = "onlyoffice";
-    runScript = "/nix/store/hx7dd7l5mby97gmyz585yxvhv5mg22wm-onlyoffice-desktopeditors-8.0.0/bin/DesktopEditors";
-    extraBwrapArgs = [ "--tmpfs /usr/share"
-                       "--symlink ${x11Fonts} /usr/share/fonts" 
-                     ];
+  mkFHSEnv = onlyofficeUnwrapped:
+    pkgs.buildFHSUserEnvBubblewrap rec {
+      name = "onlyoffice";
 
-    targetPkgs = pkgs: with pkgs; [
-      onlyofficeUnwrapped
-    ];
+      # Originally `runScript = "DesktopEditors";` This does not work for me.
+      # the error is "DesktopEditors: No such file or directory".
 
-    extraInstallCommands = ''
-      mkdir -p $out/share/applications
-      ln -s ${onlyofficeUnwrapped}/share/icons $out/share
-      cp ${onlyofficeUnwrapped}/share/applications/onlyoffice-desktopeditors.desktop $out/share/applications
-      substituteInPlace $out/share/applications/onlyoffice-desktopeditors.desktop \
-        --replace "${onlyofficeUnwrapped}/bin/onlyoffice-desktopeditors" "/nix/store/rcg087qmai5x8p3y40ww8swflc3yjjpp-onlyoffice-bwrap"
-    '';
-  });
-in
+      # My next attempt is to supply an absolute path:
+      # runScript = "${onlyofficeUnwrapped}/bin/DesktopEditors"
+      # However, I still get "No such file or directory".
 
-{
+      # A peek in /nix/store reveals that this file is in ${...}/bin.
+      # runScript = "${onlyofficeUnwrapped}/bin/onlyoffice-desktopeditors";
+      # This runs, but does not let OnlyOffice access to system fonts,
+      # whether from cmd `onlyoffice ...` or from Nautilus.
+
+      # Unfortunately, `onlyofficeUnwrapped` does not contain other binaries.
+      # So we have to find the required binary in its base package.
+      # (`onlyofficeUnwrapped` is a buildFSHEnv package.)  I have to look at
+      # the source to find how to access the original package. The attribute
+      # we need is `onlyofficeUnwrapped.args.targetPkgs`, which is a function.
+      # We have to call it with an argument (let's make it `{}`), it will
+      # return a list of packages. We find the first package whose `pname
+      # attribute is "onlyoffice-desktopeditors". The path to this package
+      # is the path we want.
+
+      # I could have used easier ways to get the package from `targetPkgs`,
+      # either `lib.lists.elemAt (onlyofficeUnwrapped.args.targetPkgs {}) 1`
+      # `builtins.head (builtins.tail (onlyofficeUnwrapped.args.targetPkgs {}))`
+      # However, both relies on that the onlyoffice package must come the
+      # second element of the list, which is a risky assumption and may break
+      # in a future version.
+
+      runScript = "${
+        lists.findFirst
+        (p: builtins.match "onlyoffice-desktopeditors" p.pname != null)
+        null
+        (onlyofficeUnwrapped.args.targetPkgs {})
+      }/bin/DesktopEditors";
+      extraBwrapArgs = [
+        "--tmpfs /usr/share"
+        "--symlink ${x11Fonts} /usr/share/fonts"
+      ];
+
+      targetPkgs = pkgs': [onlyofficeUnwrapped];
+
+      extraInstallCommands = ''
+        mkdir -p $out/share/applications
+        ln -s ${onlyofficeUnwrapped}/share/icons $out/share
+        cp ${onlyofficeUnwrapped}/share/applications/onlyoffice-desktopeditors.desktop $out/share/applications
+        substituteInPlace $out/share/applications/onlyoffice-desktopeditors.desktop \
+          --replace "${onlyofficeUnwrapped}/bin/onlyoffice-desktopeditors" "$out/bin/onlyoffice"
+      '';
+    };
+in {
   options = {
     programs.onlyoffice = {
       enable = mkOption {
@@ -61,6 +95,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ (mkFHSEnv cfg.package) ];
+    environment.systemPackages = [(mkFHSEnv cfg.package)];
   };
 }
